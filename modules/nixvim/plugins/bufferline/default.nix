4308a0740227ecc723867dc8c4ec99c25629b4b5
teevik <teemu.vikoren@gmail.com>
2024-03-09 03:20:13 +0100

    Bufferline keys

--- modules/nixvim/plugins/bufferline/default.nix
+++ modules/nixvim/plugins/bufferline/default.nix
@@ -5,6 +5,49 @@ let
 in
 {
   config = mkIf cfg.enable {
+    keymaps = [
+      {
+        mode = "n";
+        key = "gb";
+        action = "<cmd>:BufferLinePick<cr>";
+        options = {
+          silent = true;
+          desc = "Go to buffer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>bd";
+        action = "<cmd>:bd<cr>";
+        options = {
+          silent = true;
+          desc = "Delete buffer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>bn";
+        action = "<cmd>:bnext<cr>";
+        options = {
+          silent = true;
+          desc = "Next buffer";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>bp";
+        action = "<cmd>:bprevious<cr>";
+        options = {
+          silent = true;
+          desc = "Previous buffer";
+        };
+      }
+    ];
+
+    plugins.which-key.registrations = {
+      "<leader>b" = "+Buffer";
+    };
+
     plugins.bufferline = {
       offsets = [
         {
